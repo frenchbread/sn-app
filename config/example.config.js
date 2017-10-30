543069c47c25b5b385759375fc539e1e437ccc82
Damir Mustafin <damir.mustafin@protonmail.ch>
2017-10-30 16:08:22 +0200

    Add api_host env variable for production

--- config/example.config.js
+++ config/example.config.js
@@ -1,4 +1,4 @@
 module.exports = {
   dev_host: 'http://127.0.0.1:8000',
-  prod_host: ''
+  prod_host: 'http://127.0.0.1:8000'
 }
--- config/prod.env.js
+++ config/prod.env.js
@@ -1,3 +1,4 @@
 module.exports = {
-  NODE_ENV: '"production"'
+  NODE_ENV: '"production"',
+  API_HOST: `"${config.prod_host.prod_host}"`
 }
